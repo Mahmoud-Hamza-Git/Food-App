bf7d9a10e6b9f03d9fe1cdd603249bb6b01ff6fa
Mahmoud_Hamza <86957735+Mahmoud-Hamza-Git@users.noreply.github.com>
2024-03-20 09:09:28 +0200

    Adding backend Branch

--- src/utils/utilities.js
+++
@@ -1,7 +0,0 @@
-export const backendUrl = "http://localhost:3000";
-export function formatter(price) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(price);
-}
