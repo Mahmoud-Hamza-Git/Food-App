970dba92d2e99ee68c7d07a9c49628c40e54a0e4
Mahmoud_Hamza <86957735+Mahmoud-Hamza-Git@users.noreply.github.com>
2024-03-20 09:32:19 +0200

    Edit backendUrl

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,4 +1,4 @@
-export const backendUrl = "http://localhost:3000";
+export const backendUrl = "https://food-app-backend-uda3.onrender.com";
 export function formatter(price) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
